59d81df48124f439980c20f27a93db6596e4f7a7
Soham Metha <sohammetha01@gmail.com>
2025-07-13 17:06:58 +0000

    13/07/2025 : 22:36:58

--- include/B1/B1_Table.h
+++ include/B1/B1_Table.h
@@ -5,9 +5,9 @@
 
 #define MAX_PHILOSOPHER_COUNT 10
 
-void wait_startGrabingUtensils(int philosopherID);
+void wait_startGrabingUtensils();
 
-void signal_doneGrabbingUtensils(int philosopherID);
+void signal_doneGrabbingUtensils();
 
 void addThinkersToTable(int n);
 
