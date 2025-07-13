e04a4f3c9c4d7ad2edfe1016d01660e6b0836b0a
Soham Metha <sohammetha01@gmail.com>
2025-07-13 19:14:17 +0000

    14/07/2025 : 00:44:17

--- src/B1/B1_Philosopher.c
+++ src/B1/B1_Philosopher.c
@@ -1,74 +1,78 @@
 #include <B1_Table.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-const char *colors[8] = {
-    "\033[41m",  // 0 - Red
-    "\033[42m",  // 1 - Green
-    "\033[43m",  // 2 - Yellow
-    "\033[44m",  // 3 - Blue
-    "\033[45m",  // 4 - Magenta
-    "\033[46m",  // 5 - Cyan
-    "\033[102m", // 6 - Bright Green
-    "\033[103m"  // 7 - Light Yellow
-};
-const char *msg[10] = {" WAITING TO GRAB UTENSILS ",
-                       " STARTED GRABING UTENSILS ",
-                       "      PICKED UP FORK      ",
-                       "  DONE GRABBING UTENSILS  ",
-                       "          EATING          ",
-                       "        DONE EATING       ",
-                       "       PUT DOWN FORK      ",
-                       " BOTH FORKS NOT AVAILABLE ",
-                       "         THINKING         ",
-                       "       DONE THINKING      "
-};
+const char *colors[8] = {"\033[41m", "\033[42m", "\033[43m",  "\033[44m",
+                         "\033[45m", "\033[46m", "\033[102m", "\033[103m"};
+
+const char *msg[13] = {" WAITING TO GRAB UTENSILS ", " STARTED GRABING UTENSILS ", "      PICKED UP FORK      ",
+                       "  DONE GRABBING UTENSILS  ", "          EATING          ", "        DONE EATING       ",
+                       "       PUT DOWN FORK      ", " BOTH FORKS NOT AVAILABLE ", "         THINKING         ",
+                       "       DONE THINKING      ", "WAITING TO RETURN UTENSILS", "    RETURNING UTENSILS    ",
+                       "     RETURNED UTENSILS    "};
 
 #define msgFmt "\n\033[0m%s \t Philosopher %d : [ %s ] \033[0m"
+
+#define EAT()                                                                                                          \
+    {                                                                                                                  \
+        printf(msgFmt, bg, i, msg[4]);                                                                                 \
+        sleep((rand() % 10) + 1);                                                                                      \
+        printf(msgFmt, bg, i, msg[5]);                                                                                 \
+    }
+
+#define THINK()                                                                                                        \
+    {                                                                                                                  \
+        printf(msgFmt, bg, i, msg[8]);                                                                                 \
+        sleep((rand() % 10) + 1);                                                                                      \
+        printf(msgFmt, bg, i, msg[9]);                                                                                 \
+    }
 
 void *letThinkersThink(void *philosopherNo)
 {
     Philosopher *id = (Philosopher *)philosopherNo;
     int i = id->ID;
-    const char *bg = colors[i];
+    const char *bg = colors[i % 8];
 
     while (1)
     {
-        printf(msgFmt, bg, i, msg[0]);
-        wait_startGrabingUtensils();
+        THINK();
 
-        printf(msgFmt, bg, i, msg[1]);
-
-        if (areForksAvailable(i, (i + 1) % id->count))
-        {
-            wait_pickUpFork(i);
-            printf(msgFmt " Fork I%d", bg, i, msg[2], i);
-
-            wait_pickUpFork((i + 1) % id->count);
-
-            printf(msgFmt " Fork I%d", bg, i, msg[2], (i + 1) % id->count);
-
-            printf(msgFmt, bg, i, msg[3]);
-            signal_doneGrabbingUtensils();
-
-            printf(msgFmt, bg, i, msg[4]);
-            sleep(1);
-            printf(msgFmt, bg, i, msg[5]);
-
-            signal_putDownFork((i + 1) % id->count);
-            printf(msgFmt " Fork I%d", bg, i, msg[6], ((i + 1) % id->count));
-
-            signal_putDownFork(i);
-            printf(msgFmt " Fork I%d", bg, i, msg[6], i);
-        }
-        else
+        if (!areForksAvailable(i, (i + 1) % id->count))
         {
             printf(msgFmt, bg, i, msg[7]);
-            signal_doneGrabbingUtensils();
+            continue;
         }
 
-        printf(msgFmt, bg, i, msg[8]);
-        sleep(1);
-        printf(msgFmt, bg, i, msg[9]);
+        printf(msgFmt, bg, i, msg[0]);
+        wait_startGrabingUtensils();
+        //----------------------------------------------------- CRITICAL SECTION START
+        printf(msgFmt, bg, i, msg[1]);
+        wait_pickUpFork(i);
+        printf(msgFmt " Fork I%d", bg, i, msg[2], i);
+
+        wait_pickUpFork((i + 1) % id->count);
+        printf(msgFmt " Fork I%d", bg, i, msg[2], (i + 1) % id->count);
+
+        printf(msgFmt, bg, i, msg[3]);
+        //----------------------------------------------------- CRITICAL SECTION END
+        signal_doneGrabbingUtensils();
+
+        EAT();
+
+        printf(msgFmt, bg, i, msg[10]);
+        wait_startGrabingUtensils();
+        //----------------------------------------------------- CRITICAL SECTION START
+        printf(msgFmt, bg, i, msg[11]);
+
+        signal_putDownFork((i + 1) % id->count);
+        printf(msgFmt " Fork I%d", bg, i, msg[6], ((i + 1) % id->count));
+
+        signal_putDownFork(i);
+        printf(msgFmt " Fork I%d", bg, i, msg[6], i);
+
+        printf(msgFmt, bg, i, msg[3]);
+        //----------------------------------------------------- CRITICAL SECTION END
+        signal_doneGrabbingUtensils();
     }
 
     return NULL;
