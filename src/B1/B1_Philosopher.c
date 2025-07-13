c2f0a410dfb39bba4dedf376087207a131dc4292
Soham Metha <sohammetha01@gmail.com>
2025-07-13 17:07:11 +0000

    13/07/2025 : 22:37:11

--- src/B1/B1_Philosopher.c
+++ src/B1/B1_Philosopher.c
@@ -7,7 +7,7 @@ void *letThinkersThink(void *philosopherNo)
     while (1)
     {
         printf("[WAITING TO GRAB UTENSILS] : %d\n", id->ID);
-        wait_startGrabingUtensils(id->ID);
+        wait_startGrabingUtensils();
         printf("[STARTED GRABBING UTENSILS] : %d\n", id->ID);
 
         printf("[WAITING TO PICK UP FORK] : %d\n", id->ID);
@@ -18,7 +18,7 @@ void *letThinkersThink(void *philosopherNo)
         wait_pickUpFork((id->ID + 1) % id->count);
         printf("[PICKED UP FORK] : %d\n", (id->ID + 1) % id->count);
 
-        signal_doneGrabbingUtensils(id->ID);
+        signal_doneGrabbingUtensils();
         printf("[DONE GRABBING UTENSILS] : %d\n", id->ID);
 
         printf("[EATING] : %d\n", id->ID);
