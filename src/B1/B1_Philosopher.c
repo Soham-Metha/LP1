bcb84165164e6e473a5e902c26aa4e91bea5dcf7
Soham Metha <sohammetha01@gmail.com>
2025-07-13 18:01:03 +0000

    13/07/2025 : 23:31:03

--- src/B1/B1_Philosopher.c
+++ src/B1/B1_Philosopher.c
@@ -17,43 +17,43 @@ void *letThinkersThink(void *philosopherNo)
 
     while (1)
     {
-        printf("%s[WAITING TO GRAB UTENSILS]            : %sP%d\n", colors[3], bg, i);
+        printf("%s[WAITING TO GRAB UTENSILS]            : %sPhilosopher %d\n", colors[3], bg, i);
         wait_startGrabingUtensils();
 
-        printf("%s[STARTED GRABBING UTENSILS]           : %sP%d\n", colors[1], bg, i);
+        printf("%s[STARTED GRABBING UTENSILS]           : %sPhilosopher %d\n", colors[1], bg, i);
 
         if (areForksAvailable(i, (i + 1) % id->count))
         {
             wait_pickUpFork(i);
-            printf("%s[PICKED UP FORK]                      : %sP%d : Fork I%d\n", colors[1], bg, i, i);
+            printf("%s[PICKED UP FORK]                      : %sPhilosopher %d : Fork I%d\n", colors[1], bg, i, i);
 
             wait_pickUpFork((i + 1) % id->count);
-            printf("%s[PICKED UP FORK]                      : %sP%d : Fork I%d\n", colors[1], bg, i,
+            printf("%s[PICKED UP FORK]                      : %sPhilosopher %d : Fork I%d\n", colors[1], bg, i,
                    (i + 1) % id->count);
 
-            printf("%s[DONE GRABBING UTENSILS]              : %sP%d\n", colors[4], bg, i);
+            printf("%s[DONE GRABBING UTENSILS]              : %sPhilosopher %d\n", colors[4], bg, i);
             signal_doneGrabbingUtensils();
 
-            printf("%s[EATING]                              : %sP%d\n", colors[4], bg, i);
+            printf("%s[EATING]                              : %sPhilosopher %d\n", colors[4], bg, i);
             sleep(1);
-            printf("%s[DONE EATING]                         : %sP%d\n", colors[3], bg, i);
+            printf("%s[DONE EATING]                         : %sPhilosopher %d\n", colors[3], bg, i);
 
             signal_putDownFork((i + 1) % id->count);
-            printf("%s[PUT DOWN FORK]                       : %sP%d : Fork I%d\n", colors[2], bg, i,
+            printf("%s[PUT DOWN FORK]                       : %sPhilosopher %d : Fork I%d\n", colors[2], bg, i,
                    (i + 1) % id->count);
 
             signal_putDownFork(i);
-            printf("%s[PUT DOWN FORK]                       : %sP%d : Fork I%d\n", colors[2], bg, i, i);
+            printf("%s[PUT DOWN FORK]                       : %sPhilosopher %d : Fork I%d\n", colors[2], bg, i, i);
         }
         else
         {
-            printf("%s[BOTH FORKS NOT AVAILABLE]            : %sP%d\n", colors[2], bg, i);
+            printf("%s[BOTH FORKS NOT AVAILABLE]            : %sPhilosopher %d\n", colors[2], bg, i);
             signal_doneGrabbingUtensils();
         }
 
-        printf("%s[THINKING]                            : %sP%d\n", colors[3], bg, i);
+        printf("%s[THINKING]                            : %sPhilosopher %d\n", colors[3], bg, i);
         sleep(1);
-        printf("%s[DONE THINKING]                       : %sP%d\n", colors[3], bg, i);
+        printf("%s[DONE THINKING]                       : %sPhilosopher %d\n", colors[3], bg, i);
     }
 
     return NULL;
