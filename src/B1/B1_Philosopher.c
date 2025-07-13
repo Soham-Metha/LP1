abc115bea1b4a85b18e9b2d14434549b600e059a
Soham Metha <sohammetha01@gmail.com>
2025-07-13 17:05:43 +0000

    13/07/2025 : 22:35:43

--- src/B1/B1_Philosopher.c
+++ src/B1/B1_Philosopher.c
@@ -1,2 +1,33 @@
-#include <B1_Philosopher.h>
+#include <B1_Table.h>
+#include <unistd.h>
 
+void *letThinkersThink(void *philosopherNo)
+{
+    PhilosopherIdentifier *id = (PhilosopherIdentifier *)philosopherNo;
+    while (1)
+    {
+        printf("[WAITING TO GRAB UTENSILS] : %d\n", id->ID);
+        wait_startGrabingUtensils(id->ID);
+        printf("[STARTED GRABBING UTENSILS] : %d\n", id->ID);
+
+        printf("[WAITING TO PICK UP FORK] : %d\n", id->ID);
+        wait_pickUpFork(id->ID);
+        printf("[PICKED UP FORK] : %d\n", id->ID);
+
+        printf("[WAITING TO PICK UP FORK] : %d\n", (id->ID + 1) % id->count);
+        wait_pickUpFork((id->ID + 1) % id->count);
+        printf("[PICKED UP FORK] : %d\n", (id->ID + 1) % id->count);
+
+        signal_doneGrabbingUtensils(id->ID);
+        printf("[DONE GRABBING UTENSILS] : %d\n", id->ID);
+
+        printf("[EATING] : %d\n", id->ID);
+        sleep(1);
+
+        signal_putDownFork((id->ID + 1) % id->count);
+        printf("[PUT DOWN FORK] : %d\n", (id->ID + 1) % id->count);
+
+        signal_putDownFork(id->ID);
+        printf("[PUT DOWN FORK] : %d\n", id->ID);
+    }
+}
