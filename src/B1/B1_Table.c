f43e82f50b7e4087d863be51947d6d23789180d6
Soham Metha <sohammetha01@gmail.com>
2025-07-13 16:29:40 +0000

    13/07/2025 : 21:59:40

--- src/B1/B1_Table.c
+++ src/B1/B1_Table.c
@@ -1,12 +1,14 @@
 #include <B1_Table.h>
+#include <stdio.h>
 
+char someonePickingUpForkFlag = 1;
 Philosopher philosophers[MAX_PHILOSOPHER_COUNT];
 
 void addThinkersToTable(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        pthread_create(&philosophers[i], NULL, toEatOrNotToEat, NULL);
+        pthread_create(&philosophers[i], NULL, toEatOrNotToEat, i);
     }
 }
 
@@ -17,4 +19,21 @@ void removeThinkersFromTable(int n)
         pthread_cancel(philosophers[i]);
         pthread_join(philosophers[i], NULL);
     }
+}
+
+void wait_pickUpFork(int philosopherID)
+{
+    if (someonePickingUpForkFlag <= 0)
+        printf("[WAITING TO PICK UP FORK] : %d\n", philosopherID);
+
+    while (someonePickingUpForkFlag <= 0)
+        ;
+    someonePickingUpForkFlag -= 1;
+    printf("[PICKED UP FORK] : %d\n", philosopherID);
+}
+
+void signal_putDownFork(int philosopherID)
+{
+    someonePickingUpForkFlag += 1;
+    printf("[PUT DOWN FORK] : %d\n", philosopherID);
 }
