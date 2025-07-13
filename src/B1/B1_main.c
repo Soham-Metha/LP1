c9ef32fc2150a432cb8b8ad50df2b7dca2c43921
Soham Metha <sohammetha01@gmail.com>
2025-07-13 19:17:50 +0000

    14/07/2025 : 00:47:50

--- src/B1/B1_main.c
+++ src/B1/B1_main.c
@@ -1,5 +1,32 @@
 #include <B1_Table.h>
 
+Philosopher philosophers[MAX_PHILOSOPHER_COUNT];
+
+void addThinkersToTable(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        philosophers[i] = (Philosopher){.ID = i, .count = n};
+        pthread_create(&philosophers[i].thread, NULL, letThinkersThink, &philosophers[i]);
+    }
+}
+
+void removeThinkersFromTable(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        pthread_cancel(philosophers[i].thread);
+    }
+}
+
+void waitForThinkersToLeave(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        pthread_join(philosophers[i].thread, NULL);
+    }
+}
+
 int main()
 {
     int thinkerCnt = 2;
