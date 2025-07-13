54f3df347dd3cdfce4f8bc8ef40fa699f0cf5803
Soham Metha <sohammetha01@gmail.com>
2025-07-13 17:14:23 +0000

    13/07/2025 : 22:44:23

--- src/B1/B1_Philosopher.c
+++ src/B1/B1_Philosopher.c
@@ -6,28 +6,28 @@ void *letThinkersThink(void *philosopherNo)
     PhilosopherIdentifier *id = (PhilosopherIdentifier *)philosopherNo;
     while (1)
     {
-        printf("[WAITING TO GRAB UTENSILS] : %d\n", id->ID);
+        printf("[WAITING TO GRAB UTENSILS]  : %d\n", id->ID);
         wait_startGrabingUtensils();
         printf("[STARTED GRABBING UTENSILS] : %d\n", id->ID);
 
-        printf("[WAITING TO PICK UP FORK] : %d\n", id->ID);
+        printf("[WAITING TO PICK UP FORK]   : %d\n", id->ID);
         wait_pickUpFork(id->ID);
-        printf("[PICKED UP FORK] : %d\n", id->ID);
+        printf("[PICKED UP FORK]            : %d\n", id->ID);
 
-        printf("[WAITING TO PICK UP FORK] : %d\n", (id->ID + 1) % id->count);
+        printf("[WAITING TO PICK UP FORK]   : %d\n", (id->ID + 1) % id->count);
         wait_pickUpFork((id->ID + 1) % id->count);
-        printf("[PICKED UP FORK] : %d\n", (id->ID + 1) % id->count);
+        printf("[PICKED UP FORK]            : %d\n", (id->ID + 1) % id->count);
 
         signal_doneGrabbingUtensils();
-        printf("[DONE GRABBING UTENSILS] : %d\n", id->ID);
+        printf("[DONE GRABBING UTENSILS]    : %d\n", id->ID);
 
-        printf("[EATING] : %d\n", id->ID);
+        printf("[EATING]                    : %d\n", id->ID);
         sleep(1);
 
         signal_putDownFork((id->ID + 1) % id->count);
-        printf("[PUT DOWN FORK] : %d\n", (id->ID + 1) % id->count);
+        printf("[PUT DOWN FORK]             : %d\n", (id->ID + 1) % id->count);
 
         signal_putDownFork(id->ID);
-        printf("[PUT DOWN FORK] : %d\n", id->ID);
+        printf("[PUT DOWN FORK]             : %d\n", id->ID);
     }
 }
