20679953e0c42858fc4c65d31c5e06f5f253647b
Soham Metha <sohammetha01@gmail.com>
2025-07-13 19:32:00 +0000

    14/07/2025 : 01:02:00

--- src/B1/B1_Fork.c
+++ src/B1/B1_Fork.c
@@ -4,7 +4,7 @@ char someonePickingUpForkFlag = 1;
 
 Fork forks[MAX_PHILOSOPHER_COUNT];
 
-void addUtensilsToTable(int n)
+inline void addUtensilsToTable(int n)
 {
     for (int i = 0; i < n; i++)
         forks[i] = 1;
@@ -15,26 +15,26 @@ char areForksAvailable(int a, int b)
     return forks[a] && forks[b];
 }
 
-void wait_pickUpFork(int ForkNo)
+inline void wait_pickUpFork(int ForkNo)
 {
     while (forks[ForkNo] <= 0)
         ;
     forks[ForkNo] -= 1;
 }
 
-void signal_putDownFork(int ForkNo)
+inline void signal_putDownFork(int ForkNo)
 {
     forks[ForkNo] += 1;
 }
 
-void wait_startGrabingUtensils()
+inline void wait_startGrabingUtensils()
 {
     while (someonePickingUpForkFlag <= 0)
         ;
     someonePickingUpForkFlag -= 1;
 }
 
-void signal_doneGrabbingUtensils()
+inline void signal_doneGrabbingUtensils()
 {
     someonePickingUpForkFlag += 1;
 }
