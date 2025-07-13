2941f17002ae4287e49b786a9b054df57a07860c
Soham Metha <sohammetha01@gmail.com>
2025-07-13 17:00:47 +0000

    13/07/2025 : 22:30:47

--- src/B1/B1_Fork.c
+++ src/B1/B1_Fork.c
@@ -4,17 +4,12 @@ Fork forks[MAX_PHILOSOPHER_COUNT];
 
 void wait_pickUpFork(int ForkNo)
 {
-    if (forks[ForkNo] <= 0)
-        printf("[WAITING TO PICK UP FORK] : %d\n", ForkNo);
-
     while (forks[ForkNo] <= 0)
         ;
     forks[ForkNo] -= 1;
-    printf("[PICKED UP FORK] : %d\n", ForkNo);
 }
 
 void signal_putDownFork(int ForkNo)
 {
     forks[ForkNo] += 1;
-    printf("[PUT DOWN FORK] : %d\n", ForkNo);
 }
