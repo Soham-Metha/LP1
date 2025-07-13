41b3856b5a4b279cc5a8e9fac65c7ecdb40bb6a7
Soham Metha <sohammetha01@gmail.com>
2025-07-13 17:18:05 +0000

    13/07/2025 : 22:48:05

--- src/B1/B1_Philosopher.c
+++ src/B1/B1_Philosopher.c
@@ -6,28 +6,28 @@ void *letThinkersThink(void *philosopherNo)
     PhilosopherIdentifier *id = (PhilosopherIdentifier *)philosopherNo;
     while (1)
     {
-        printf("[WAITING TO GRAB UTENSILS]  : %d\n", id->ID);
+        printf("\n\t[WAITING TO GRAB UTENSILS]  : P%d", id->ID);
         wait_startGrabingUtensils();
-        printf("[STARTED GRABBING UTENSILS] : %d\n", id->ID);
+        printf("\n\t[STARTED GRABBING UTENSILS] : P%d", id->ID);
 
-        printf("[WAITING TO PICK UP FORK]   : %d\n", id->ID);
+        printf("\n\t[WAITING TO PICK UP FORK]   : P%d  :  I%d", id->ID, id->ID);
         wait_pickUpFork(id->ID);
-        printf("[PICKED UP FORK]            : %d\n", id->ID);
+        printf("\n\t[PICKED UP FORK]            : P%d  :  I%d", id->ID, id->ID);
 
-        printf("[WAITING TO PICK UP FORK]   : %d\n", (id->ID + 1) % id->count);
+        printf("\n\t[WAITING TO PICK UP FORK]   : P%d  :  I%d", id->ID, (id->ID + 1) % id->count);
         wait_pickUpFork((id->ID + 1) % id->count);
-        printf("[PICKED UP FORK]            : %d\n", (id->ID + 1) % id->count);
+        printf("\n\t[PICKED UP FORK]            : P%d  :  I%d", id->ID, (id->ID + 1) % id->count);
 
         signal_doneGrabbingUtensils();
-        printf("[DONE GRABBING UTENSILS]    : %d\n", id->ID);
+        printf("\n\t[DONE GRABBING UTENSILS]    : P%d", id->ID);
 
-        printf("[EATING]                    : %d\n", id->ID);
+        printf("\n\t[EATING]                    : P%d", id->ID);
         sleep(1);
 
         signal_putDownFork((id->ID + 1) % id->count);
-        printf("[PUT DOWN FORK]             : %d\n", (id->ID + 1) % id->count);
+        printf("\n\t[PUT DOWN FORK]             : P%d  :  I%d", id->ID, (id->ID + 1) % id->count);
 
         signal_putDownFork(id->ID);
-        printf("[PUT DOWN FORK]             : %d\n", id->ID);
+        printf("\n\t[PUT DOWN FORK]             : P%d  :  I%d", id->ID, id->ID);
     }
 }
