1cfb80efb3c8cfb2050b36ee5f9deda25e9a6ed7
Soham Metha <sohammetha01@gmail.com>
2025-07-17 14:44:25 +0000

    17/07/2025 : 20:14:25

--- src/A1/A1_parser.c
+++ src/A1/A1_parser.c
@@ -1,3 +1,37 @@
 #include <A1_parser.h>
+#include <string.h>
 
-void randomFunctionToIgnoreEmptyFileWarning();
+char *line[MAX_LINE_WIDTH];
+
+void processLabel()
+{
+    char *labelNm = line;
+    while (*line[0] != ' ' || *line[0] != '\t')
+    {
+        (*line)++;
+    }
+    (*line)[0] = '\0';
+    (*line)++;
+}
+
+void processInstrcution()
+{
+    printf("%s", *line);
+}
+
+void processLine()
+{
+    if (*line[0] != ' ' || *line[0] != '\t')
+    {
+        processLabel();
+    }
+    processInstruction();
+}
+
+void processFile()
+{
+    while (fgets(*line, MAX_LINE_WIDTH, stdin) != NULL)
+    {
+        processLine();
+    }
+}
