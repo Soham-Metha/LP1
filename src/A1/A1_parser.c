2dd0b9379be27eb3f790206191069508e71a2233
Soham Metha <sohammetha01@gmail.com>
2025-07-17 15:37:43 +0000

    17/07/2025 : 21:07:43

--- src/A1/A1_parser.c
+++ src/A1/A1_parser.c
@@ -1,37 +1,5 @@
 #include <A1_parser.h>
-#include <string.h>
-typedef struct String
-{
-    const char *data;
-    int length;
-} String;
-
-void trim(String *str)
-{
-    while (str->data[0] == ' ' || str->data[0] == '\t')
-    {
-        str->data += 1;
-        str->length -= 1;
-    }
-
-    while (str->data[str->length - 1] == ' ' || str->data[str->length - 1] == '\t' ||
-           str->data[str->length - 1] == '\n')
-    {
-        str->length -= 1;
-    }
-}
-
-String getNextToken(String *line)
-{
-    String token = (String){.data = line->data, .length = 0};
-    while (line->data[0] != ' ' && line->data[0] != '\t')
-    {
-        token.length += 1;
-        line->data += 1;
-        line->length -= 1;
-    }
-    return token;
-}
+#include <A1_tokens.h>
 
 void processLine(String *line)
 {
