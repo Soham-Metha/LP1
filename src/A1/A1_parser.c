0d2888de1e54d9f1f9285e1339ce5f6cac5dbe45
Soham Metha <sohammetha01@gmail.com>
2025-07-17 19:08:23 +0000

    18/07/2025 : 00:38:23

--- src/A1/A1_parser.c
+++ src/A1/A1_parser.c
@@ -6,7 +6,7 @@ int IP = 0;
 
 void printInstructionDetailsAndExecuteAssemblerDirectives(Instruction inst)
 {
-    printf("\n%-4d\t(", IP);
+    printf("\n%-2d\t(", IP);
     switch (inst.memonic)
     {
     case INST_IS_MOVER:
