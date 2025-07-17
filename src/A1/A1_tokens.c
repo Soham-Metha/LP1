6877ee3d7968d7592c5641ee31813005384864f7
Soham Metha <sohammetha01@gmail.com>
2025-07-17 15:37:22 +0000

    17/07/2025 : 21:07:22

---
+++ src/A1/A1_tokens.c
@@ -0,0 +1,28 @@
+#include <A1_tokens.h>
+
+void trim(String *str)
+{
+    while (str->data[0] == ' ' || str->data[0] == '\t')
+    {
+        str->data += 1;
+        str->length -= 1;
+    }
+
+    while (str->data[str->length - 1] == ' ' || str->data[str->length - 1] == '\t' ||
+           str->data[str->length - 1] == '\n')
+    {
+        str->length -= 1;
+    }
+}
+
+String getNextToken(String *line)
+{
+    String token = (String){.data = line->data, .length = 0};
+    while (line->data[0] != ' ' && line->data[0] != '\t')
+    {
+        token.length += 1;
+        line->data += 1;
+        line->length -= 1;
+    }
+    return token;
+}
