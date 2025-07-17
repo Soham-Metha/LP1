1b85c5264972fa42f69f8c92216b0f5dc8056d7c
Soham Metha <sohammetha01@gmail.com>
2025-07-17 13:37:33 +0000

    17/07/2025 : 19:07:33

---
+++ include/A1/A1_operands.h
@@ -0,0 +1,29 @@
+#pragma once
+
+typedef char OperandType;
+
+typedef enum Register
+{
+    REG_AREG = 1,
+    REG_BREG,
+    REG_CREG,
+    REG_DREG,
+    REGISTER_CNT
+} Register;
+
+typedef enum Conditions
+{
+    COND_LT = 1,
+    CONT_LE,
+    COND_EQ,
+    COND_GT,
+    COND_GE,
+    COND_ANY,
+    COND_CNT
+} Conditions;
+
+typedef struct OperandLUT
+{
+    OperandType value;
+    const char *name;
+} OperandLUT;
