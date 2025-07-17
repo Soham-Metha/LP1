48f662890bba1cc0a73551af4dc4c93df4c75ff6
Soham Metha <sohammetha01@gmail.com>
2025-07-17 14:21:54 +0000

    17/07/2025 : 19:51:54

--- include/A1/A1_memonics.h
+++ include/A1/A1_memonics.h
@@ -11,7 +11,7 @@ typedef enum InstType
 
 typedef enum Memonic
 {
-    INST_IS_STOP = INST_TYPE_IS << 4 + 0,
+    INST_IS_STOP = (INST_TYPE_IS << 4) + 0,
     INST_IS_ADD,
     INST_IS_SUB,
     INST_IS_MULT,
@@ -22,12 +22,12 @@ typedef enum Memonic
     INST_IS_DIV,
     INST_IS_READ,
     INST_IS_PRINT,
-    INST_AD_START = INST_TYPE_AD << 4 + 1,
+    INST_AD_START = (INST_TYPE_AD << 4) + 1,
     INST_AD_END,
     INST_AD_ORIGIN,
     INST_AD_EQU,
     INST_AD_LTORG,
-    INST_DL_DC = INST_TYPE_DL << 4 + 1,
+    INST_DL_DC = (INST_TYPE_DL << 4) + 1,
     INST_DL_DS,
     INSTRUCTION_CNT
 } Memonic;
