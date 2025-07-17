01ce8f442f6908c3e230d33f6d8c8cda8fc7fed4
Soham Metha <sohammetha01@gmail.com>
2025-07-17 14:17:49 +0000

    17/07/2025 : 19:47:49

---
+++ src/A1/A1_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define MAX_LINE_WIDTH 100
+
+int main()
+{
+    char line[MAX_LINE_WIDTH];
+
+    while (fgets(line, MAX_LINE_WIDTH, stdin) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    return 0;
+}
