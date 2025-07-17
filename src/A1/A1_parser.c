d664534910c2e377c02093e9cda0339ebfdeb8d4
Soham Metha <sohammetha01@gmail.com>
2025-07-17 18:54:40 +0000

    18/07/2025 : 00:24:40

--- src/A1/A1_parser.c
+++ src/A1/A1_parser.c
@@ -4,16 +4,37 @@
 
 int IP = 0;
 
-void printInstructionDetailsAndExecuteAssemblerDirectives(MemonicType memo)
+void printInstructionDetailsAndExecuteAssemblerDirectives(Instruction inst)
 {
     printf("\n (");
-    if (memo >= INST_TYPE_DL << 4)
-        printf("DL, ");
-    else if (memo >= INST_TYPE_AD << 4)
-        printf("AD, ");
-    else
+    switch (inst.memonic)
+    {
+    case INST_IS_MOVER:
+    case INST_IS_MOVEM:
+    case INST_IS_PRINT:
+    case INST_IS_STOP:
+    case INST_IS_MULT:
+    case INST_IS_COMP:
+    case INST_IS_READ:
+    case INST_IS_ADD:
+    case INST_IS_SUB:
+    case INST_IS_DIV:
+    case INST_IS_BC:
         printf("IS, ");
-    printf("%d)", memo & 0x0F);
+        break;
+    case INST_AD_END:
+    case INST_AD_EQU:
+    case INST_AD_START:
+    case INST_AD_LTORG:
+    case INST_AD_ORIGIN:
+        printf("AD, ");
+        break;
+    case INST_DL_DC:
+    case INST_DL_DS:
+        printf("DL, ");
+        break;
+    }
+    printf("%d)", inst.memonic & 0x0F);
 }
 
 void processLabel(String *line)
@@ -28,26 +49,28 @@ void processInstruction(String *line)
     trim(line);
     Token tok = getNextToken(line, LINE_INST);
     MemonicType memoID = getMemonicIdFromName(tok.value);
-    printInstructionDetailsAndExecuteAssemblerDirectives(memoID);
 
     unsigned char oprCnt = getOperandCountFromId(memoID);
+    Operand opr[2];
     for (unsigned char i = 0; i < oprCnt; i++)
     {
-        Operand opr;
         Token oprTok = getNextToken(line, LINE_INST);
         if (oprTok.type == TOKEN_CONST)
         {
             // printf(" (C, %.*s)", oprTok.value.length, oprTok.value.data);
-            opr = (Operand){.type = OPERAND_CONST, .as_const = oprTok.value};
+            opr[i] = (Operand){.type = OPERAND_CONST, .as_const = oprTok.value};
         }
-        else if (getOperandIdFromName(oprTok.value, &opr))
+        else if (getOperandIdFromName(oprTok.value, &opr[i]))
         {
             // printf(" (%d)", opr.as_condID);
         }
         else
         {
-            opr = (Operand){.type = OPERAND_SYMBOL, .as_symbolID = searchOrInsertInSymTab(oprTok.value)};
+            opr[i] = (Operand){.type = OPERAND_SYMBOL, .as_symbolID = searchOrInsertInSymTab(oprTok.value)};
             // printf(" (S, %d)", id);
         }
     }
+
+    printInstructionDetailsAndExecuteAssemblerDirectives(
+        (Instruction){.memonic = memoID, .operand1 = opr[0], .operand2 = opr[2]});
 }
