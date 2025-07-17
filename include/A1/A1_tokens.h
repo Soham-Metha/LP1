46815f3ae577bc8d627a099307ad2accd10cfe3e
Soham Metha <sohammetha01@gmail.com>
2025-07-17 16:58:25 +0000

    17/07/2025 : 22:28:25

--- include/A1/A1_tokens.h
+++ include/A1/A1_tokens.h
@@ -1,11 +1,21 @@
 #pragma once
 #include <string.h>
 
+typedef enum LineType
+{
+    LINE_LABEL,
+    LINE_INST,
+    LINE_CNT,
+} LineType;
+
 typedef enum TokenType
 {
     TOKEN_LABEL,
     TOKEN_MEMONIC,
-    TOKEN_OPERAND,
+    TOKEN_CONST,
+    TOKEN_SYMBOL,
+    TOKEN_REGISTER,
+    TOKEN_COMPARISON,
     TOKEN_CNT
 } TokenType;
 
