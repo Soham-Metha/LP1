47e4031bbcc6a0e110076f40e212167f91aa0bac
Soham Metha <sohammetha01@gmail.com>
2025-07-21 13:30:21 +0000

    21/07/2025 : 19:00:21

--- src/A1/A1_instructions.c
+++ src/A1/A1_instructions.c
@@ -45,6 +45,7 @@ void printSymTab()
 {
     for (OperandType i = 0; i < table.tableSize; i++)
     {
-        printf("\n\t %-2d | %d : '%.*s'", i, table.table[i].addr, table.table[i].name.length, table.table[i].name.data);
+        printf("\n\t %-2d | %d\t: '%.*s'", i, table.table[i].addr, table.table[i].name.length,
+               table.table[i].name.data);
     }
 }
