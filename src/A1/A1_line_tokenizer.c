2aa7ee7afea9741c5c996a72efee7892d5ccd283
Soham Metha <sohammetha01@gmail.com>
2025-07-21 15:43:47 +0000

    21/07/2025 : 21:13:47

--- src/A1/A1_line_tokenizer.c
+++ src/A1/A1_line_tokenizer.c
@@ -19,26 +19,18 @@ void trim(String *str)
 Token getNextToken(String *line, LineType type)
 {
     trim(line);
+    TokenType toktype = TOKEN_CNT;
     String val = (String){.data = line->data, .length = 0};
     switch (line->data[0])
     {
     case '=':
-        line->data += 2;
-        line->length -= 2;
-        val.data += 2;
-        while (line->data[0] != '\'')
-        {
-            if (line->length == 0)
-            {
-                printf("\nCOULDNT FIND A CLOSING ' \' '");
-            }
-            line->data += 1;
-            line->length -= 1;
-            val.length += 1;
-        }
-        return (Token){.type = TOKEN_LIT, .value = val};
-
+        toktype = TOKEN_LIT;
+        line->data += 1;
+        line->length -= 1;
+        val.data += 1;
     case '\'':
+        if (toktype != TOKEN_LIT)
+            toktype = TOKEN_CONST;
         line->data += 1;
         line->length -= 1;
         val.data += 1;
@@ -52,9 +44,9 @@ Token getNextToken(String *line, LineType type)
             line->length -= 1;
             val.length += 1;
         }
-        return (Token){.type = TOKEN_CONST, .value = val};
-    default: {
-        TokenType toktype = (type == LINE_LABEL) ? TOKEN_LABEL : TOKEN_NAME;
+        return (Token){.type = toktype, .value = val};
+    default:
+        toktype = (type == LINE_LABEL) ? TOKEN_LABEL : TOKEN_NAME;
         if ((line->data[0] >= '0' && line->data[0] <= '9') || line->data[0] == '-')
             toktype = TOKEN_CONST;
         while (line->data[0] != ' ' && line->data[0] != '\t' && line->data[0] != '\n' && line->data[0] != '\0')
@@ -64,6 +56,5 @@ Token getNextToken(String *line, LineType type)
             val.length += 1;
         }
         return (Token){.type = toktype, .value = val};
-    }
     }
 }
