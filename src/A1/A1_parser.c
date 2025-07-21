0a7a24a2dcc263d36861ca5a24c9c55a99e3d9ff
Soham Metha <sohammetha01@gmail.com>
2025-07-21 14:42:05 +0000

    21/07/2025 : 20:12:05

--- src/A1/A1_parser.c
+++ src/A1/A1_parser.c
@@ -24,7 +24,6 @@ void processInstruction(String *line)
         }
         else if (!getOperandIdFromName(oprTok.value, &opr[i]))
         {
-            // printf("%.*s", oprTok.value.length, oprTok.value.data);
             opr[i] = (Operand){.type = OPERAND_SYMBOL, .as_symbolID = searchOrInsertInSymTab(oprTok.value)};
         }
     }
