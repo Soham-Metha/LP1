2aa1e648a3ea58ce3a05845557bc2e0f19edcadb
Soham Metha <sohammetha01@gmail.com>
2025-07-21 13:02:32 +0000

    21/07/2025 : 18:32:32

--- src/A1/A1_instructions.c
+++ src/A1/A1_instructions.c
@@ -10,7 +10,6 @@ OperandType searchOrInsertInSymTab(String symbol)
         if (table.table[i].name.length == symbol.length &&
             strncmp(table.table[i].name.data, symbol.data, symbol.length) == 0)
         {
-            printf("%.*s-%.*s", table.table[i].name.length, table.table[i].name.data, symbol.length, symbol.data);
             return i;
         }
     }
@@ -25,6 +24,7 @@ void UpdateAddressInSymTab(String symbol, int address)
         if (table.table[i].name.length == symbol.length &&
             strncmp(table.table[i].name.data, symbol.data, symbol.length) == 0)
         {
+            printf("%.*s-%.*s", table.table[i].name.length, table.table[i].name.data, symbol.length, symbol.data);
             table.table[i].addr = address;
             return;
         }
