f9bf2332e9e8d063896e89a6750e302772466288
Soham Metha <sohammetha01@gmail.com>
2025-07-21 13:04:34 +0000

    21/07/2025 : 18:34:34

--- src/A1/A1_instructions.c
+++ src/A1/A1_instructions.c
@@ -13,9 +13,9 @@ OperandType searchOrInsertInSymTab(String symbol)
             return i;
         }
     }
-    printf("%.*s-%.*s", table.table[table.tableSize].name.length, table.table[table.tableSize].name.data, symbol.length,
-           symbol.data);
     table.table[table.tableSize++] = (SymTabEntry){.name = symbol};
+    printf("%.*s-%.*s", table.table[table.tableSize - 1].name.length, table.table[table.tableSize - 1].name.data,
+           symbol.length, symbol.data);
     return table.tableSize - 1;
 }
 
