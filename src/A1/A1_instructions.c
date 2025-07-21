5e85f7a47aff9258dcc43518f8283c73182af78e
Soham Metha <sohammetha01@gmail.com>
2025-07-21 13:29:38 +0000

    21/07/2025 : 18:59:38

--- src/A1/A1_instructions.c
+++ src/A1/A1_instructions.c
@@ -8,8 +8,8 @@ OperandType searchOrInsertInSymTab(String symbol)
 {
     for (OperandType i = 0; i < table.tableSize; i++)
     {
-        printf("\ncompared %.*s-%.*s", table.table[i].name.length, table.table[i].name.data, symbol.length,
-               symbol.data);
+        // printf("\ncompared %.*s-%.*s", table.table[i].name.length, table.table[i].name.data, symbol.length,
+        //        symbol.data);
         if (table.table[i].name.length == symbol.length &&
             (strncmp(table.table[i].name.data, symbol.data, symbol.length) == 0))
         {
@@ -18,6 +18,7 @@ OperandType searchOrInsertInSymTab(String symbol)
     }
     char *deepcopy = malloc(symbol.length);
     memcpy(deepcopy, symbol.data, symbol.length);
+
     table.table[table.tableSize++] = (SymTabEntry){.name = {.data = deepcopy, .length = symbol.length}};
     printf("\nINSERTED '%.*s'-'%.*s'", table.table[table.tableSize - 1].name.length,
            table.table[table.tableSize - 1].name.data, symbol.length, symbol.data);
