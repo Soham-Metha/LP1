22cea5898e78994f9570b277208c15644cccad67
Soham Metha <sohammetha01@gmail.com>
2025-07-21 13:39:22 +0000

    21/07/2025 : 19:09:22

--- src/A1/A1_directives.c
+++ src/A1/A1_directives.c
@@ -63,15 +63,15 @@ void printInstructionDetailsAndExecuteAssemblerDirectives(Instruction inst)
     {
         if (inst.operand1.type == OPERAND_COND)
         {
-            printf(" \t(%d)", inst.operand1.as_condID);
+            printf(" | (%d)", inst.operand1.as_condID);
         }
         else if (inst.operand1.type == OPERAND_CONST)
         {
-            printf(" \t(C, %.*s)", inst.operand1.as_const.length, inst.operand1.as_const.data);
+            printf(" | (C, %.*s)", inst.operand1.as_const.length, inst.operand1.as_const.data);
         }
         else
         {
-            printf(" \t(S, %d)", inst.operand1.as_symbolID);
+            printf(" | (S, %d)", inst.operand1.as_symbolID);
         }
     }
     if (oprCnt > 1)
