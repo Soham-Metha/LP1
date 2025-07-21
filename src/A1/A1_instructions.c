9e432c619de3be6715da570ba63df224071289a5
Soham Metha <sohammetha01@gmail.com>
2025-07-21 12:56:39 +0000

    21/07/2025 : 18:26:39

--- src/A1/A1_instructions.c
+++ src/A1/A1_instructions.c
@@ -8,7 +8,7 @@ OperandType searchOrInsertInSymTab(String symbol)
     for (OperandType i = 0; i < table.tableSize; i++)
     {
         if (table.table[i].name.length == symbol.length &&
-            strncmp(table.table[i].name.data, symbol.data, symbol.length))
+            strncmp(table.table[i].name.data, symbol.data, symbol.length) == 0)
         {
             return i;
         }
